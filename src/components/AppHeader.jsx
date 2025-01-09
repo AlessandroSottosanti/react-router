92ff39314ae2a10779a94c0985f7a28c9c58f3fd
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-09 16:33:38 +0100

    feat: navlink color

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -18,12 +18,16 @@ function AppHeader() {
     ]
 
     return (
-        <header className="py-3 bg-primary">
+        <header className="py-3 bg-primary d-flex">
+            <div className="d-flex justify-content-center align-self-center text-white mx-3">
+                    <span className="logo">MyBlog!</span>
+                </div>
             <nav className="container d-flex justify-content-center">
+                
                 <ul className="nav d-flex gap-3">
                     {menu.map((page) => (
                         <li className="nav-item" key={page.title}>
-                            <NavLink className="nav-link text-white" to={page.path}>
+                            <NavLink className="nav-link" to={page.path}>
                                 {page.title}
                             </NavLink>
                         </li>

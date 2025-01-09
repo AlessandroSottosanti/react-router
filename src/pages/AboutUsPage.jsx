82f7aec9ace7b498a3904c92b8714ebc28666766
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-09 16:56:10 +0100

    feat: details About us

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -3,10 +3,12 @@ function AboutUsPage() {
         <main className="my-3">
             <div className="container">
                 <h1>MyBlog:</h1>
-                <p>Siamo un'azienda giovane che punta a bal bla bla bla bla bla, bla . bla bla bla</p>
-                <p>ed in fine Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, sit blanditiis.
-                     Doloremque quia inventore aut, similique, 
+                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, sit blanditiis.
+                    Doloremque quia inventore aut, similique,
                     eveniet esse odio alias in eaque molestiae iusto suscipit ipsum at, assumenda aperiam cumque?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae harum sunt beatae laborum, 
+                    accusamus velit possimus dicta facere culpa hic numquam quasi modi, temporibus quisquam neque iusto 
+                    repellendus minus necessitatibus!</p>
             </div>
         </main>
     )

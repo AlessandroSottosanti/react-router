39367b6ada33ccd5da06b9095cd8daee0ea9700f
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-09 14:50:08 +0100

    feat: style nav

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,25 +4,30 @@ function AppHeader() {
     const menu = [
         {
             path: "/",
-            title:"Home"
+            title: "Home"
         },
         {
             path: "/blogs",
             title: "Blogs"
         }
-        
+
     ]
 
     return (
-        <header className="d-flex justify-content-center align-items-center py-3 bg-primary">
-            <ul className="d-flex gap-3">
-                {menu.map((page) => (
-                    <li key={page.title}>
-                        <NavLink to={page.path}>{page.title}</NavLink>
-                    </li>
-                ))}
-            </ul>
+        <header className="py-3 bg-primary">
+            <nav className="container d-flex justify-content-center">
+                <ul className="nav d-flex gap-3">
+                    {menu.map((page) => (
+                        <li className="nav-item" key={page.title}>
+                            <NavLink className="nav-link text-white" to={page.path}>
+                                {page.title}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
+
     )
 }
 
